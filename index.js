e1ec86b9fad7cb264ad1b390c1b747194ac4ba9c
ChethanTN7 <chethantn947@gmail.com>
2024-08-22 18:53:11 +0530

    done with get, create, update the user

--- index.js
+++ index.js
@@ -13,11 +13,96 @@ app.get("/", (req, res) => {
   });
 });
 
+// get all the users
 app.get("/users", (req, res) => {
   res.status(200).json({
     success: true,
     data: users,
   });
+});
+
+// get user by id
+app.get("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User dosn't exist",
+    });
+  }
+  return res.status(200).json({
+    success: true,
+    message: "user found",
+    data: user,
+  });
+});
+
+// creating a new user
+app.post("/users", (req, res) => {
+  const { id, name, surname, email, subscriptionType, subscriptionDate } =
+    req.body;
+  const user = users.find((each) => each.id === id);
+  if (user) {
+    return res.status(404).json({
+      success: false,
+      message: "User already exist",
+    });
+  }
+  users.push({
+    id,
+    name,
+    surname,
+    email,
+    subscriptionType,
+    subscriptionDate,
+  });
+  return res.status(201).json({
+    success: true,
+    message: "User added",
+    data: users,
+  });
+});
+
+// updating the user by id
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const { data } = req.body;
+  const user = users.find((each) => each.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User dosn't exist",
+    });
+  }
+  const updatedUserData = users.map((each) => {
+    if (each.id === id) {
+      return {
+        ...each,
+        ...data,
+      };
+    }
+    return each;
+  });
+  res.status(200).json({
+    success: true,
+    message: "User updated successfully",
+    data: updatedUserData,
+  });
+});
+
+// Deleting the user by id
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const user = users.find((each) => each.id == id);
+  if (!user) {
+    return res.status(404).json({
+      success: false,
+      message: "User dosn't exist",
+    });
+  }
+
+  // delete code here ...
 });
 
 app.get("*", (req, res) => {
